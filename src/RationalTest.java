479fffcd7741bce2868185242b2486e8438ddeed
Prateek Vachher <vachh007@umn.edu>
2018-01-31 10:52:02 -0600

    Testing class yo

--- src/RationalTest.java
+++ src/RationalTest.java
@@ -4,6 +4,11 @@ public class RationalTest {
 
     @org.junit.Test
     public void add() {
+        Rational a = new Rational(1,2);
+        Rational b = new Rational(1,4);
+        a.add(b);
+        assertTrue(6 == a.getNum());
+        assertTrue(8 == a.getDenom());
     }
 
     @org.junit.Test
@@ -12,6 +17,11 @@ public class RationalTest {
 
     @org.junit.Test
     public void mul() {
+        Rational a = new Rational(1,2);
+        Rational b = new Rational(1,4);
+        a.mul(b);
+        assertTrue(1 == a.getNum());
+        assertTrue(8 == a.getDenom());
     }
 
     @org.junit.Test
