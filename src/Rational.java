4145485f48232e3f538d817adcc8d8aafda95efe
Prateek Vachher <vachh007@umn.edu>
2018-01-31 10:40:42 -0600

    Created Idea Project

---
+++ src/Rational.java
@@ -0,0 +1,43 @@
+public class Rational {
+    private int num;
+    private int denom;
+
+    public Rational (int num, int denom){
+        this.num = num;
+        this.denom = denom;
+    }
+
+    public void setNum (int num){
+        num = num;
+    }
+    public void setDenom (int denom){
+        denom = denom;
+    }
+
+    public int getNum (){
+        return num;
+    }
+    public int getDenom () {
+        return denom;
+    }
+
+    public void add(Rational r){
+        this.num = (this.num * r.denom) + (r.num * this.denom);
+        this.denom = this.denom * r.denom;
+    }
+
+    public void sub(Rational r){
+        this.num = (this.num * r.denom) - (r.num * this.denom);
+        this.denom = this.denom * r.denom;
+    }
+
+    public void mul(Rational r){
+        this.num = this.num * r.num;
+        this.denom = this.denom * r.denom;
+    }
+
+    public void div(Rational r){
+        this.num = this.num * r.denom;
+        this.denom = this.denom*r.num;
+    }
+}
